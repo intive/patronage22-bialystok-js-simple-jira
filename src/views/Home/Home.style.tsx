941ca20bc70dc2a7e8f5cd4bc9e1f94f381a94d2
Voland91 <you@example.com>
2022-01-12 22:41:48 +0100

    added some styles

--- src/views/Home/Home.style.tsx
+++ src/views/Home/Home.style.tsx
@@ -3,6 +3,9 @@ import styled from "styled-components";
 export const StyledHomeWrapper = styled.section`
   height: 100vh;
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
+  padding: 20px;
+  text-align: center;
 `;
--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -3,6 +3,8 @@ import { Button } from "../../components/Button/Button";
 
 export const Home = () => (
   <StyledHomeWrapper>
-    <Button child="GO TO ANOTHER SITE!" />
+    <p>If button is nice and smooth - Material UI works</p>
+    <Button child="Click here to check if React Router works" />
+    <p>If colors are black and white - Styled components works</p>
   </StyledHomeWrapper>
 );
