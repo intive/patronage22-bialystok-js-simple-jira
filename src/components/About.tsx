9c72994bfba336d5e34869f5d1e69a6c35c8a244
unknown <adamowicz.piotr2@gmail.com>
2022-01-12 14:55:42 +0100

    Initial commit

---
+++ src/components/About.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+interface Props {}
+
+const About = (props: Props) => {
+  return (
+    <div>
+      <h1>About Page</h1>
+    </div>
+  );
+};
+
+export default About;
