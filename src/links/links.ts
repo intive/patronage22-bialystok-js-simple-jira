4851c30fc0716113d26f9ac96bfdb3286fd3d3ae
Jakub Prymak <jprymak@gmail.com>
2022-01-20 19:40:32 +0100

    create a separate file for defining links

---
+++ src/links/links.ts
@@ -0,0 +1,18 @@
+interface objectType {
+  id: number;
+  url: string;
+  text: string;
+}
+
+export const links: objectType[] = [
+  {
+    id: 0,
+    url: "/",
+    text: "home",
+  },
+  {
+    id: 1,
+    url: "/second",
+    text: "second",
+  },
+];
