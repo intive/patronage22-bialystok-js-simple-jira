3785e2e8befe0afc0b8f5bb2088a51c801805f9f
unknown <adamowicz.piotr2@gmail.com>
2022-01-28 12:58:52 +0100

    Storybook with LinearProgress Component

--- src/components/ProgressBar/ProgressBar.tsx
+++ src/components/ProgressBar/ProgressBar.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Box from "@mui/material/Box";
 import LinearProgress from "@mui/material/LinearProgress";
+// import { LinearProgressProps } from "/@mui/material";
 
 export default function LinearIndeterminate() {
   return (
-    <Box sx={{ width: "100%" }}>
-      <LinearProgress />
+    <Box sx={{ width: "100%", px: 2 }}>
+      <LinearProgress sx={{ width: "80%", mx: "auto" }} />
     </Box>
   );
 }
