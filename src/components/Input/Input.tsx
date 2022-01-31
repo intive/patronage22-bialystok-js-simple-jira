305bc0a6a1cbb07d02fba290b713483cb7db51a4
Ludwig <workingpeter@wp.pl>
2022-01-31 18:09:22 +0100

    Adding Input

---
+++ src/components/Input/Input.tsx
@@ -0,0 +1,32 @@
+import { BaseTextFieldProps } from "@mui/material";
+import { ChangeEventHandler } from "react";
+import { StyledTextField } from "./Input.style";
+
+interface Input extends BaseTextFieldProps {
+  value: string;
+  rows?: string;
+  required?: boolean;
+  onChangeHandler?: ChangeEventHandler<HTMLTextAreaElement | HTMLInputElement>;
+}
+
+const Input = ({
+  value,
+  rows,
+  required,
+  onChangeHandler,
+  ...extraProps
+}: Input) => {
+  return (
+    <StyledTextField
+      value={value}
+      onChange={onChangeHandler}
+      multiline={Boolean(rows)}
+      rows={rows}
+      fullWidth
+      required={required}
+      {...extraProps}
+    />
+  );
+};
+
+export default Input;
