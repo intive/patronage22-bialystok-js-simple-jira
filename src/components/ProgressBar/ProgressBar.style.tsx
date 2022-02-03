c2ab955272428d7c96edd78391662c02194652c0
unknown <adamowicz.piotr2@gmail.com>
2022-02-03 20:53:18 +0100

    fix styling; storybook not working with new styling engine

---
+++ src/components/ProgressBar/ProgressBar.style.tsx
@@ -0,0 +1,13 @@
+import { styled } from "@mui/material/styles";
+import ProgressBar from "./ProgressBar";
+import Box from "@mui/material/Box";
+
+export const StyledProgressBar = styled(ProgressBar)({
+  width: "80%",
+  mx: "auto",
+});
+
+export const StyledBox = styled(Box)({
+  width: "100%",
+  px: 2,
+});
--- src/components/ProgressBar/ProgressBar.tsx
+++ src/components/ProgressBar/ProgressBar.tsx
@@ -1,12 +1,9 @@
-import React from "react";
-import Box from "@mui/material/Box";
-import LinearProgress from "@mui/material/LinearProgress";
-// import { LinearProgressProps } from "/@mui/material";
+import { StyledProgressBar, StyledBox } from "./ProgressBar.style";
 
-export default function LinearIndeterminate() {
+export default function ProgressBar() {
   return (
-    <Box sx={{ width: "100%", px: 2 }}>
-      <LinearProgress sx={{ width: "80%", mx: "auto" }} />
-    </Box>
+    <StyledBox>
+      <StyledProgressBar />
+    </StyledBox>
   );
 }
