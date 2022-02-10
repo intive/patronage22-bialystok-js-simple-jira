a1ba6971e4c5af4a78d128112ed78870ea4956b4
Ludwig <workingpeter@wp.pl>
2022-02-10 17:16:08 +0100

    Removing PageHeader component

--- src/views/Owl/Owl.tsx
+++ src/views/Owl/Owl.tsx
@@ -1,11 +1,9 @@
 import { Box } from "@mui/material";
 import Input from "../../components/Input/Input";
-import PageHeader from "../../components/PageHeader/PageHeader";
 
 export const Owl_components = () => {
   return (
     <Box>
-      <PageHeader />
       <Input helperText='Label of something' value='' />
     </Box>
   );
