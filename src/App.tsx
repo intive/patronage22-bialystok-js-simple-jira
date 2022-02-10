e9812b1ce8601d908588c20ffb67cac1bf8980f2
Ludwig <workingpeter@wp.pl>
2022-02-10 12:52:46 +0100

    App fix

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ const App = () => (
     <ThemeProvider theme={theme}>
       <Input helperText='Shiteeet' value='' />
 
-      {/* <Select /> */}
+      <Select />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='second' element={<SecondPage />} />
