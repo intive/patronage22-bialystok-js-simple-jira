470d641b7f4901e33305e38c63e220db55f6f4e0
Karol Ciesluk <karolciesluk.db@gmail.com>
2022-02-13 14:47:01 +0100

    create storybook for ProjectCard

---
+++ src/stories/ProjectCard.stories.tsx
@@ -0,0 +1,37 @@
+import { ComponentStory } from "@storybook/react";
+import { ThemeProvider } from "@mui/material";
+
+import ProjectCard from "../components/ProjectCard";
+import ThreeDotsMenu from "../components/ThreeDotsMenu/ThreeDotsMenu";
+import { theme } from "../theme/mainTheme";
+import { mockMenuItems } from "../mockData/menuItems";
+
+export default {
+  title: "ProjectCard",
+  component: ProjectCard,
+};
+
+const Template: ComponentStory<typeof ProjectCard> = (args) => (
+  <ThemeProvider theme={theme}>
+    <ProjectCard {...args} />
+  </ThemeProvider>
+);
+
+export const WithoutMenuComponent = Template.bind({});
+
+WithoutMenuComponent.args = {
+  name: "Awesome project",
+};
+
+export const WithThreeDotsMenu = Template.bind({});
+
+WithThreeDotsMenu.args = {
+  name: "Awesome project",
+  menuComponent: <ThreeDotsMenu menuItems={mockMenuItems} />,
+};
+
+WithoutMenuComponent.parameters = WithThreeDotsMenu.parameters = {
+  controls: {
+    include: ["name"],
+  },
+};
