354f2735975a1463a3301890d330e1c2d9932591
Ludwig <workingpeter@wp.pl>
2022-02-13 21:38:20 +0100

    Started to create a story

---
+++ src/stories/Input.stories.tsx
@@ -0,0 +1,13 @@
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import Input from "../components/Input/Input";
+
+export default {
+  title: "Input_Component",
+  component: Input,
+};
+
+const Template: ComponentStory<typeof Input> = (args: any) => (
+  <Input {...args} />
+);
+
+export const InputStory = Template.bind({});
