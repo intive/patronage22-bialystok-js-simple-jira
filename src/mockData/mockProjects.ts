02b4e2db9864825d71def095455f51f5021cc356
unknown <adamowicz.piotr2@gmail.com>
2022-02-19 18:28:13 +0100

    add id to projects

--- src/mockData/mockProjects.ts
+++ src/mockData/mockProjects.ts
@@ -1,9 +1,9 @@
 export const mockProjects = [
-  { name: "Project1" },
-  { name: "Death Star" },
-  { name: "Conquer The World" },
-  { name: "Branch Party Project" },
-  { name: "Find out who killed Kennedy" },
-  { name: "Catch Them All" },
-  { name: "Watermelon Jam" },
+  { name: "Project1", id: "1" },
+  { name: "Death Star", id: "2" },
+  { name: "Conquer The World", id: "3" },
+  { name: "Branch Party Project", id: "4" },
+  { name: "Find out who killed Kennedy", id: "5" },
+  { name: "Catch Them All", id: "6" },
+  { name: "Watermelon Jam", id: "7" },
 ];
