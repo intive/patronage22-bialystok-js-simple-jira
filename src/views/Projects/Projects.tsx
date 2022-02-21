bf58b09ae96113ef7466ac96127342852f8764c5
unknown <adamowicz.piotr2@gmail.com>
2022-02-21 12:04:54 +0100

    add removing Project from view list functionality

--- src/views/Projects/Projects.tsx
+++ src/views/Projects/Projects.tsx
@@ -2,15 +2,38 @@ import PageHeader from "../../components/PageHeader/PageHeader";
 import ProjectCard from "../../components/ProjectCard";
 import ThreeDotsMenu from "../../components/ThreeDotsMenu/ThreeDotsMenu";
 import { StyledProjectList, StyledPageWrapper } from "./Projects.style";
-import { mockMenuItems } from "../../mockData/menuItems";
 import { mockProjects } from "../../mockData/mockProjects";
 import { useTranslation } from "react-i18next";
 import Grid from "@mui/material/Grid";
+import { useState } from "react";
+import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
+import ViewWeekOutlinedIcon from "@mui/icons-material/ViewWeekOutlined";
+import { ConfirmationDialog } from "../../components/ConfirmationDialog/ConfirmationDialog";
 
 export const Projects = () => {
   const { t } = useTranslation();
+  const [projects, setProjects] = useState(mockProjects);
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+  const [current, setCurrent] = useState(0);
+
+  const deleteProjectHandler = (id: string) => {
+    const newProjectsList = projects.filter((element) => element.id !== id);
+    console.log(id);
+    setProjects(newProjectsList);
+  };
+
   return (
     <StyledPageWrapper>
+      <ConfirmationDialog
+        confirmHandler={() => {
+          deleteProjectHandler(projects[current].id);
+          setIsDialogOpen(false);
+        }}
+        isOpen={isDialogOpen}
+        handleClose={() => setIsDialogOpen(false)}
+      >
+        {t("deleteProjectWarning")}
+      </ConfirmationDialog>
       <PageHeader
         pageTitle={t("projectsViewTitle")}
         buttonText={t("newProjectBtn")}
@@ -18,10 +41,30 @@ export const Projects = () => {
       />
       <StyledProjectList>
         <Grid container spacing={3}>
-          {mockProjects.map((project, id) => (
+          {projects.map((project, id) => (
             <Grid key={id} item xs={12} sm={12} md={6} lg={4} xl={3}>
               <ProjectCard
-                menuComponent={<ThreeDotsMenu menuItems={mockMenuItems} />}
+                menuComponent={
+                  <ThreeDotsMenu
+                    menuItems={[
+                      {
+                        id: 0,
+                        icon: <ViewWeekOutlinedIcon />,
+                        label: "Add column",
+                        onClick: () => console.log("column added"),
+                      },
+                      {
+                        id: 1,
+                        icon: <DeleteOutlineIcon />,
+                        label: "Delete project",
+                        onClick: () => {
+                          setIsDialogOpen(true);
+                          setCurrent(id);
+                        },
+                      },
+                    ]}
+                  />
+                }
                 name={project.name}
               />
             </Grid>
