db37b2c39efd1ca54c777febf4e19a65f6f8174e
Ludwig <workingpeter@wp.pl>
2022-02-22 15:26:27 +0100

    Deleting unused types from Input

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -4,29 +4,18 @@ import { StyledTextField, StyledInputLabel } from "./Input.style";
 
 export interface Input extends BaseTextFieldProps {
   labelHelperText?: string;
-  value?: string;
-  rows?: number;
-  required?: boolean;
-  error?: boolean;
-  helperText?: string;
   onChangeHandler?: ChangeEventHandler<HTMLTextAreaElement | HTMLInputElement>;
 }
 
-const Input = ({
-  labelHelperText,
-  // value,
-  onChangeHandler,
-  ...extraProps
-}: Input) => {
+const Input = ({ labelHelperText, onChangeHandler, ...extraProps }: Input) => {
   return (
     <>
       <StyledInputLabel htmlFor='styled-text-input'>
         {labelHelperText}
       </StyledInputLabel>
       <StyledTextField
-        // value={value}
         onChange={onChangeHandler}
-        // disabled
+        disabled
         id='styled-text-input'
         {...extraProps}
       />
