c3d1a97b7290e40b9ba64776cac7515da5566513
Karol Ciesluk <karolciesluk.db@gmail.com>
2022-02-27 15:54:33 +0100

    delete unneccesary comments

--- src/state/storeWithToolkit.ts
+++ src/state/storeWithToolkit.ts
@@ -11,14 +11,3 @@ export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
 
 export default store;
-
-// redux-thunk added automatically
-// redux dev tools automatically configured
-
-// 'createSlice' simplifies creating actions, action-creators and reducer
-// 'createSlice' automatically creates actions and returns them in property 'actions'
-// 'slice' of app state
-// thanks to 'immer' library we can write reducers code without worrying about Immutability
-// 'nanoid' function is available out of the box
-
-// https://redux-toolkit.js.org/tutorials/typescript
--- src/views/SecondPage/countSlice.ts
+++ src/views/SecondPage/countSlice.ts
@@ -23,11 +23,3 @@ export const selectCountValue = (state: RootState) =>
   selectCountState(state).count;
 
 export default countSlice.reducer;
-
-// 'createSlice' simplifies creating actions, action-creators and reducer
-// 'createSlice' automatically creates actions and returns them in property 'actions'
-// 'slice' of app state
-// thanks to 'immer' library we can write reducers code without worrying about Immutability
-// 'nanoid' function is available out of the box
-
-// https://redux-toolkit.js.org/tutorials/typescript
