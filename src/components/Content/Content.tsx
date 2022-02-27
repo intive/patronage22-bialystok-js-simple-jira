3b279ef325a165e31d7f9eafe0cdcf2ae601e67b
Karol Ciesluk <karolciesluk.db@gmail.com>
2022-02-27 07:11:18 +0100

    create Content component for dealing with loading datas

---
+++ src/components/Content/Content.tsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import { Navigate } from "react-router-dom";
+
+import { toHome } from "../../routes";
+import { LoaderWrapper, StyledLoader } from "./Content.styled";
+
+interface ContentProps {
+  isLoading: boolean;
+  noContentToShow: boolean;
+  children: React.ReactNode;
+}
+
+const Content = ({ isLoading, noContentToShow, children }: ContentProps) => {
+  if (isLoading) {
+    return (
+      <LoaderWrapper>
+        <StyledLoader />
+      </LoaderWrapper>
+    );
+  }
+
+  if (noContentToShow) {
+    return <Navigate to={toHome} />;
+  }
+
+  return <>{children}</>;
+};
+
+export default Content;
