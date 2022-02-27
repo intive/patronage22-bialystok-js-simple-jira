bc873226f4c31f2a6cf9879df6a6f92730876882
Karol Ciesluk <karolciesluk.db@gmail.com>
2022-02-27 20:31:28 +0100

    delete unnecessary comments

--- src/views/Projects/Projects.tsx
+++ src/views/Projects/Projects.tsx
@@ -61,26 +61,4 @@ export const Projects = () => {
     default:
       return null;
   }
-
-  // return (
-  //   <StyledPageWrapper>
-  //     <PageHeader
-  //       pageTitle={t("projectsViewTitle")}
-  //       buttonText={t("newProjectBtn")}
-  //       buttonHandler={() => console.log("works")}
-  //     />
-  //     <StyledProjectList>
-  //       <Grid container spacing={3}>
-  //         {mockProjects.map((project, id) => (
-  //           <Grid key={id} item xs={12} sm={12} md={6} lg={4} xl={3}>
-  //             <ProjectCard
-  //               menuComponent={<ThreeDotsMenu menuItems={mockMenuItems} />}
-  //               name={project.name}
-  //             />
-  //           </Grid>
-  //         ))}
-  //       </Grid>
-  //     </StyledProjectList>
-  //   </StyledPageWrapper>
-  // );
 };
