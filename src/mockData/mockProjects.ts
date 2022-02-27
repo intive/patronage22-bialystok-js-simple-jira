e1d5bd7b34b17b07da4cbcff4bf9bfab1efb8842
Karol Ciesluk <karolciesluk.db@gmail.com>
2022-02-27 07:11:47 +0100

    update loginc in Project component

--- src/mockData/mockProjects.ts
+++ src/mockData/mockProjects.ts
@@ -1,9 +1,9 @@
 export const mockProjects = [
-  { name: "Project1" },
-  { name: "Death Star" },
-  { name: "Conquer The World" },
-  { name: "Branch Party Project" },
-  { name: "Find out who killed Kennedy" },
-  { name: "Catch Them All" },
-  { name: "Watermelon Jam" },
+  { id: 1, name: "Project1" },
+  { id: 2, name: "Death Star" },
+  { id: 3, name: "Conquer The World" },
+  { id: 4, name: "Branch Party Project" },
+  { id: 5, name: "Find out who killed Kennedy" },
+  { id: 6, name: "Catch Them All" },
+  { id: 7, name: "Watermelon Jam" },
 ];
--- src/views/Projects/Projects.tsx
+++ src/views/Projects/Projects.tsx
@@ -3,31 +3,37 @@ import ProjectCard from "../../components/ProjectCard";
 import ThreeDotsMenu from "../../components/ThreeDotsMenu/ThreeDotsMenu";
 import { StyledProjectList, StyledPageWrapper } from "./Projects.style";
 import { mockMenuItems } from "../../mockData/menuItems";
-import { mockProjects } from "../../mockData/mockProjects";
 import { useTranslation } from "react-i18next";
 import Grid from "@mui/material/Grid";
+import { useProjects } from "./useProjects";
+import Content from "../../components/Content/Content";
 
 export const Projects = () => {
+  const [isLoading, projects] = useProjects();
   const { t } = useTranslation();
+
   return (
-    <StyledPageWrapper>
-      <PageHeader
-        pageTitle={t("projectsViewTitle")}
-        buttonText={t("newProjectBtn")}
-        buttonHandler={() => console.log("works")}
-      />
-      <StyledProjectList>
-        <Grid container spacing={3}>
-          {mockProjects.map((project, id) => (
-            <Grid key={id} item xs={12} sm={12} md={6} lg={4} xl={3}>
-              <ProjectCard
-                menuComponent={<ThreeDotsMenu menuItems={mockMenuItems} />}
-                name={project.name}
-              />
-            </Grid>
-          ))}
-        </Grid>
-      </StyledProjectList>
-    </StyledPageWrapper>
+    <Content isLoading={isLoading} noContentToShow={projects.length === 0}>
+      <StyledPageWrapper>
+        <PageHeader
+          pageTitle={t("projectsViewTitle")}
+          buttonText={t("newProjectBtn")}
+          buttonHandler={() => console.log("works")}
+        />
+
+        <StyledProjectList>
+          <Grid container spacing={3}>
+            {projects.map((project) => (
+              <Grid key={project.id} item xs={12} sm={12} md={6} lg={4} xl={3}>
+                <ProjectCard
+                  menuComponent={<ThreeDotsMenu menuItems={mockMenuItems} />}
+                  name={project.name}
+                />
+              </Grid>
+            ))}
+          </Grid>
+        </StyledProjectList>
+      </StyledPageWrapper>
+    </Content>
   );
 };
