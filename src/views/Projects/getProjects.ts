dd23f8cdff524d87762097da02c58f73360c0aa3
Karol Ciesluk <karolciesluk.db@gmail.com>
2022-02-27 20:23:40 +0100

    delete getProjects function

--- src/views/Projects/getProjects.ts
+++
@@ -1,9 +0,0 @@
-export const getProjects = async () => {
-  const response = await fetch("./exampleProjects.json");
-
-  if (!response.ok) {
-    new Error(response.statusText);
-  }
-
-  return await response.json();
-};
