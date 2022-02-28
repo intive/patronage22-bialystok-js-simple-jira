839224d6f149209d3a5e3ff41e8ce6b4a803d60e
alicjakarczewska <alicja.karczewska1@gmail.com>
2022-02-28 11:10:09 +0100

    remove comments from files

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,41 +1,7 @@
-// const path = require("path");
-// // const { TsconfigPathsPlugin } = require('tsconfig-paths-webpack-plugin');
-
-// module.exports = {
-//   "webpackFinal": async (config) => {
-//     // config.resolve.alias['~'] = path.resolve(__dirname, '../src/');
-//     config.resolve.alias['@'] = path.resolve(__dirname, '../src');
-//     return config;
-//   },
-//   stories: [
-//     "../src/**/*.stories.mdx",
-//     "../src/**/*.stories.@(js|jsx|ts|tsx)"
-//   ],
-//   addons: [
-//     "@storybook/addon-links",
-//     "@storybook/addon-essentials",
-//     "@storybook/preset-create-react-app"
-//   ],
-//   framework: "@storybook/react",
-//   core: {
-//     builder: "webpack5",
-//   },
-//   // webpackFinal: async (config) => {
-//   //   config.resolve.plugins = [new TsconfigPathsPlugin({ extensions: config.resolve.extensions })]
-//   //   return config
-//   // },
-// }
-
 const path = require("path");
-// const { TsconfigPathsPlugin } = require('tsconfig-paths-webpack-plugin');
 const { alias } = require("react-app-rewire-alias");
 
 module.exports = {
-  // "webpackFinal": async (config) => {
-  //   // config.resolve.alias['~'] = path.resolve(__dirname, '../src/');
-  //   config.resolve.alias['@'] = path.resolve(__dirname, '../src');
-  //   return config;
-  // },
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
     "@storybook/addon-links",
