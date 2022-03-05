6b06149833d93400c1b27907a240f0e20dd4cea6
Jakub Prymak <jprymak@gmail.com>
2022-03-05 23:48:05 +0100

    change basic modal props so that it cannot be styled directly with them

--- src/components/BasicModal/BasicModal.style.tsx
+++ src/components/BasicModal/BasicModal.style.tsx
@@ -1,97 +1,22 @@
 import { styled } from "@mui/material/styles";
-
-import Box from "@mui/material/Box";
 import Dialog from "@mui/material/Dialog";
-import { DialogTitle } from "@mui/material";
 
-type DialogProps = {
-  paddings: Array<number>;
-};
+export const StyledDialog = styled(Dialog)({
+  overflowY: "auto",
 
-type DialogTitleProps = {
-  alignTitle: "center" | "end" | "start";
-  titleMargin: string;
-};
-
-type ButtonBoxProps = {
-  buttonsTopMargin: string;
-};
-
-export const IconBox = styled(Box)(({ theme }) => ({
-  position: "absolute",
-  top: "-60px",
-  left: "50%",
-  transform: "translateX(-50%)",
-  boxSizing: "border-box",
-  minWidth: "112px",
-  minHeight: "112px",
-  display: "grid",
-  placeItems: "center",
-  borderRadius: "50%",
-  background: theme.palette.grey["100"],
-
-  "& .MuiSvgIcon-root": {
-    width: "64px",
-    height: "64px",
-    color: theme.palette.grey["500"],
+  "& .MuiDialog-container": {
+    height: "auto",
+    minHeight: "100vh",
   },
-}));
 
-export const StyledDialog = styled(Dialog)<DialogProps>(
-  ({ paddings, theme }) => ({
-    overflowY: "auto",
-
-    "& .MuiDialog-container": {
-      height: "auto",
-      minHeight: "100vh",
-    },
-
-    "& .MuiPaper-root": {
-      boxSizing: "border-box",
-      marginBottom: "68px",
-      marginTop: "68px",
-      minWidth: "544px",
-      minHeight: "304px",
-      display: "flex",
-      alignItems: "center",
-      flexDirection: "column",
-      paddingTop: theme.spacing(paddings[0]),
-      paddingRight: theme.spacing(paddings[1]),
-      paddingBottom: theme.spacing(paddings[2]),
-      paddingLeft: theme.spacing(paddings[3]),
-      borderRadius: "8px",
-      overflow: "visible",
-      boxShadow: "0px 8px 16px rgba(98, 98, 98, 0.24)",
-    },
-
-    "& .MuiDialogContent-root": {
-      padding: 0,
-      minWidth: "100%",
-      width: 0,
-    },
-  })
-);
-
-export const ButtonBox = styled(({ buttonsTopMargin, ...props }) => (
-  <Box {...props} />
-))<ButtonBoxProps>(({ buttonsTopMargin }) => ({
-  alignSelf: "flex-end",
-  marginTop: buttonsTopMargin,
-  boxShadow: "none",
-  border: "none",
-
-  "& .MuiButtonBase-root:first-of-type": {
-    marginRight: "5px",
+  "& .MuiPaper-root": {
+    minWidth: "544px",
+    overflow: "visible",
+    boxShadow: "0px 8px 16px rgba(98, 98, 98, 0.24)",
   },
-}));
 
-export const StyledDialogTitle = styled(
-  ({ alignTitle, titleMargin, ...props }) => <DialogTitle {...props} />
-)<DialogTitleProps>(({ theme, alignTitle, titleMargin }) => ({
-  width: "100%",
-  textAlign: alignTitle,
-  lineHeight: "32px",
-  fontSize: "24px",
-  padding: 0,
-  marginBottom: titleMargin,
-}));
+  "& .MuiDialogContent-root": {
+    minWidth: "100%",
+    width: 0,
+  },
+});
--- src/components/BasicModal/BasicModal.tsx
+++ src/components/BasicModal/BasicModal.tsx
@@ -1,54 +1,24 @@
-import {
-  StyledDialog,
-  IconBox,
-  ButtonBox,
-  StyledDialogTitle,
-} from "./BasicModal.style";
-import DialogContent from "@mui/material/DialogContent";
-
+import { StyledDialog } from "./BasicModal.style";
 export interface BasicModalProps {
-  headerIcon?: JSX.Element;
   children?: JSX.Element | JSX.Element[];
-  buttons: JSX.Element[];
   handleClose?: () => void;
   isOpen: boolean;
-  /**paddings are calculated by taking mui theme spacing
-   * property and multiplying it by values in Array, calculated paddings are set clockwise, just like in normal css*/
-  paddings: Array<number>;
-  title?: string;
-  alignTitle: "center" | "end" | "start";
-  /** Bottom title margin*/
-  titleMargin?: number;
-  buttonsTopMargin?: number;
 }
 
 export default function BasicModal({
   children,
-  headerIcon,
   isOpen,
   handleClose,
-  paddings,
-  title,
-  alignTitle,
-  titleMargin = 16,
-  buttons,
-  buttonsTopMargin = 32,
 }: BasicModalProps) {
   return (
     <StyledDialog
-      paddings={paddings}
       open={isOpen}
       onClose={handleClose}
       PaperProps={{
         elevation: 2,
       }}
     >
-      {headerIcon && <IconBox>{headerIcon}</IconBox>}
-      <StyledDialogTitle alignTitle={alignTitle} titleMargin={titleMargin}>
-        {title}
-      </StyledDialogTitle>
-      <DialogContent>{children}</DialogContent>
-      <ButtonBox buttonsTopMargin={buttonsTopMargin}>{buttons}</ButtonBox>
+      {children}
     </StyledDialog>
   );
 }
