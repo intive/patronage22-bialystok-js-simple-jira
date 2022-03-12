2d31d9420c591cd8e7d90cb0d942469c7c76adc1
Ludwig <workingpeter@wp.pl>
2022-03-12 16:49:16 +0100

    Creating custom hooks in hooks folder

--- src/components/PrivateRoute/index.tsx
+++ src/components/PrivateRoute/index.tsx
@@ -1,52 +1,7 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { Navigate, useLocation } from "react-router-dom";
-import { getLocalStorage, setLocalStorage } from "src/utils/localStorage";
-
-const useLocalStorageAuth = (
-  defaultValue: boolean,
-  key: string
-): [boolean, Function] => {
-  const [value, setValue] = useState<boolean>(() => {
-    const localStorageValue = getLocalStorage(key);
-    console.log(localStorageValue);
-
-    if (localStorageValue) {
-      return JSON.parse(localStorageValue);
-    }
-
-    setLocalStorage(key, defaultValue);
-    return defaultValue;
-  });
-
-  const setAuthValue = (value: boolean) => {
-    setValue(value);
-    localStorage.setItem(key, JSON.stringify(value));
-  };
-
-  return [value, setAuthValue];
-};
-
-const useLocalStoragePathname = (
-  defaultValue: string,
-  key: string
-): [string, Function] => {
-  const [value, setValue] = useState<string>(() => {
-    const localStorageValue = getLocalStorage(key);
-    console.log(localStorageValue);
-
-    if (localStorageValue) {
-      return localStorageValue;
-    }
-    setLocalStorage(key, defaultValue);
-    return defaultValue;
-  });
-  const setLocalStorageValue = (value: string) => {
-    setValue(value);
-    localStorage.setItem(key, value);
-  };
-
-  return [value, setLocalStorageValue];
-};
+import { useLocalStorageAuth } from "src/hooks/useLocalStorageAuth";
+import { useLocalStoragePathname } from "src/hooks/useLocalStoragePathname";
 
 const PrivateRoute = ({ children }: { children: JSX.Element }) => {
   let location = useLocation();
