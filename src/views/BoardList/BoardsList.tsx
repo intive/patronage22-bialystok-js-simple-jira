cfda7de47d1ca4bb418bd70337829fe9b5d60faf
Mateusz Butkiewicz <butkiewicz.mat@gmail.com>
2022-03-12 19:30:41 +0100

    deleted unnessesary code

--- src/views/BoardList/BoardsList.tsx
+++ src/views/BoardList/BoardsList.tsx
@@ -1,69 +1,24 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { StyledBoardList, StyledPageWrapper } from "./BoardsList.style";
 import { useTranslation } from "react-i18next";
 import Grid from "@mui/material/Grid";
 import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
 import ViewWeekOutlinedIcon from "@mui/icons-material/ViewWeekOutlined";
-import { ConfirmationDialog } from "@modules/ConfirmationDialog/ConfirmationDialog";
 import PageHeader from "@modules/PageHeader/PageHeader";
 import { BoardCard } from "@components/BoardCard/BoardCard";
 import ThreeDotsMenu from "@components/ThreeDotsMenu/ThreeDotsMenu";
-import Content from "@components/Content/Content";
 import { Button } from "@components/Button/Button";
 import { mockBoardsList } from "../../mockData/mocBoardsList";
 import { useParams } from "react-router-dom";
 
-let FetchProjectsAPI: any;
-
-async function importApiModule() {
-  if (localStorage["USE_MOCK"] === "true") {
-    const module = await import("../../api/projects/mockProjectsApi");
-    FetchProjectsAPI = module.default;
-  } else {
-    const module = await import("../../api/projects/projectsApi");
-    FetchProjectsAPI = module.default;
-  }
-}
-
 export const BoardsList = () => {
-  const [isLoading, setIsLoading] = useState(true);
   const [boardsList, setBoardsList] = useState(mockBoardsList);
-  const [isDialogOpen, setIsDialogOpen] = useState(false);
-  const [current, setCurrent] = useState(0);
   const { projectID: projectName } = useParams();
 
   const { t } = useTranslation();
 
-  //   const deleteProjectHandler = (id: string) => {
-  //     const newProjectsList = projects.filter(
-  //       (element: any) => element.id !== id
-  //     );
-  //     setProjects(newProjectsList);
-  //   };
-
-  //   useEffect(() => {
-  //     async function fetchProjects() {
-  //       await importApiModule();
-  //       const projects = await FetchProjectsAPI.getProjects();
-  //       setProjects(projects);
-
-  //       setIsLoading(false);
-  //     }
-  //     fetchProjects();
-  //   }, []);
-
   return (
     <StyledPageWrapper>
-      {/* <ConfirmationDialog
-          confirmHandler={() => {
-            deleteProjectHandler(projects[current].id);
-            setIsDialogOpen(false);
-          }}
-          isOpen={isDialogOpen}
-          handleClose={() => setIsDialogOpen(false)}
-        >
-          {t("deleteProjectWarning")}
-        </ConfirmationDialog> */}
       <PageHeader
         pageTitle={`${t("labelProject")} ${projectName}`}
         interactiveElement={
@@ -89,11 +44,8 @@ export const BoardsList = () => {
                       {
                         id: 1,
                         icon: <DeleteOutlineIcon />,
-                        label: "Delete project",
-                        onClick: () => {
-                          setIsDialogOpen(true);
-                          setCurrent(id);
-                        },
+                        label: "Delete board",
+                        onClick: () => console.log("board deleted"),
                       },
                     ]}
                   />
