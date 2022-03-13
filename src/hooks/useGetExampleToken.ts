866e8a352f6accf5d3da19e2d0828c7bd6c4dd71
Karol Ciesluk <karolciesluk.db@gmail.com>
2022-03-13 19:45:41 +0100

    update example token

--- src/hooks/useGetExampleToken.ts
+++ src/hooks/useGetExampleToken.ts
@@ -3,7 +3,8 @@ import { AuthContext, logIn, logOut } from "src/contexts/authentication";
 
 const LOGIN = "testLogin";
 const PASSWORD = "password789";
-const EXAMPLE_TOKEN = "ASDFS";
+const EXAMPLE_TOKEN =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c";
 
 export interface Credentials {
   login: string;
