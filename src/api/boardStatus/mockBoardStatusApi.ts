5463aaed206dd1483539a4abe7514425f6f7fb18
Jakub Prymak <jprymak@gmail.com>
2022-03-23 21:12:37 +0100

    make mockBoardStatus api to look more like real api

--- src/api/boardStatus/mockBoardStatusApi.ts
+++ src/api/boardStatus/mockBoardStatusApi.ts
@@ -1,8 +1,9 @@
-import { mockBoardStatus } from "../../mockData/mockBoardStatus";
+import { mockBoardStatus, mockStatus } from "../../mockData/mockBoardStatus";
 import { mockIssues } from "../../mockData/mockIssues";
 
-const status = [...mockBoardStatus];
-const issues = [...mockIssues];
+interface DataObject {
+  [key: string]: any;
+}
 
 function sleep(ms = 1000) {
   return new Promise((resolve) => {
@@ -14,12 +15,46 @@ const MockBoardStatusAPI = {
   getBoardStatusById: async function (id: number) {
     await sleep();
     console.log("Fetching board status... response.status:", 200);
-    return [...status];
+    console.log("Fetching status... response.status:", 200);
+    const boardStatus = mockBoardStatus;
+    const status = mockStatus;
+
+    const boardStatusFilteredById = boardStatus.data.filter((boardStatus) => {
+      return boardStatus.boardId == 1;
+    });
+
+    if (boardStatusFilteredById.length < 1) {
+      return [];
+    } else {
+      const boardStatusIds = boardStatusFilteredById.map((boardStatus) => {
+        return boardStatus.statusId;
+      });
+
+      const statusFilteredByBoardStatusId = status.data.filter(
+        (status: DataObject) => {
+          return boardStatusIds.includes(status.id);
+        }
+      );
+
+      const statusObject: DataObject = {};
+
+      statusFilteredByBoardStatusId.forEach((obj: any) => {
+        statusObject[obj.id] = obj;
+      });
+
+      const boardStatusFilteredByIdWithStatus = boardStatusFilteredById.map(
+        (boardStatus: any) => {
+          return { ...boardStatus, status: statusObject[boardStatus.statusId] };
+        }
+      );
+
+      return boardStatusFilteredByIdWithStatus;
+    }
   },
   getIssuesByBoardStatusId: async function (id: number) {
     await sleep();
     console.log("Fetching issues... response.status:", 200);
-    return [...issues];
+    return [...mockIssues];
   },
 };
 
--- src/mockData/mockBoardStatus.tsx
+++ src/mockData/mockBoardStatus.tsx
@@ -1,5 +1,15 @@
-export const mockBoardStatus = [
-  { boardId: 1, statusId: 1, status: { code: "To do", id: 1 } },
-  { boardId: 2, statusId: 2, status: { code: "in progress", id: 2 } },
-  { boardId: 3, statusId: 3, status: { code: "done", id: 3 } },
-];
+export const mockBoardStatus = {
+  data: [
+    { boardId: 1, statusId: 1 },
+    { boardId: 1, statusId: 2 },
+    { boardId: 1, statusId: 3 },
+  ],
+};
+
+export const mockStatus = {
+  data: [
+    { code: "To do", id: 1 },
+    { code: "in progress", id: 2 },
+    { code: "done", id: 3 },
+  ],
+};
