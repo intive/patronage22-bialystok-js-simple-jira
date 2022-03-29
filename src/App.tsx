a19d5ae6cf5dcec4d6188e093fbff2b1e9934cdc
unknown <adamowicz.piotr2@gmail.com>
2022-03-29 12:42:29 +0200

    minor import fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import { Projects } from "./views/Projects/Projects";
-import { BoardsListView } from "./views/BoardList/BoardsListView";
+import { BoardsListView } from "./views/BoardView/BoardsListView";
 import { Board } from "./views/Board/Board";
 import Navbar from "./components/Navbar/Navbar";
 import PrivateRoute from "./components/PrivateRoute";
