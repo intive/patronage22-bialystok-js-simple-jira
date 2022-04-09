9f45882ccf17e3eaf89c734d17a5fff9c1821f18
Karol Ciesluk <karolciesluk.db@gmail.com>
2022-04-09 23:53:06 +0200

    write some basic projectPage e2e tests

--- cypress/support/index.d.ts
+++ cypress/support/index.d.ts
@@ -1,1 +1,11 @@
 import "@testing-library/cypress/add-commands";
+
+declare global {
+  namespace Cypress {
+    interface Chainable {
+      login(): Chainable<Element>;
+      fetchProjectsFromApi(): Chainable<Element>;
+      fetchEmptyProjectList(): Chainable<Element>;
+    }
+  }
+}
