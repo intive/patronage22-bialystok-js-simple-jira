0938703750a0fd542e3c896b7a622cf96025f810
Jakub Prymak <jprymak@gmail.com>
2022-04-09 22:30:21 +0200

    remove add column option from project card

--- src/views/Projects/Projects.tsx
+++ src/views/Projects/Projects.tsx
@@ -149,7 +149,6 @@ export const Projects = () => {
                 setIsDltDialogOpen(true);
                 setCurrent(projectId);
               }}
-              addColumnHandler={() => console.log("column added")}
             />
           )}
         </StyledPageWrapper>
