9f81577f016fd0783e552d32ba92507568a454de
Jakub Prymak <jprymak@gmail.com>
2022-04-10 16:42:08 +0200

    display app language based on browser language settings

--- src/translations/i18n.tsx
+++ src/translations/i18n.tsx
@@ -3,12 +3,21 @@ import { initReactI18next } from "react-i18next";
 import en from "./en.json";
 import pl from "./pl.json";
 
+const checkBrowserLanguage = () => {
+  const currentBrowserLanguage = navigator.language;
+  if (currentBrowserLanguage === "en" || currentBrowserLanguage === "en-US") {
+    return "en";
+  } else {
+    return "pl";
+  }
+};
+
 i18next.use(initReactI18next).init({
   react: {
     transWrapTextNodes: "span",
   },
   debug: true,
-  fallbackLng: "pl",
+  fallbackLng: checkBrowserLanguage(),
   interpolation: {
     escapeValue: false,
   },
